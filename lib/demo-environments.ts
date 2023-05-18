1829d3724d5fc182d0e8129e61e464ae75707703
Nikhil Vengal <ngvengal@gmail.com>
2023-05-17 17:15:54 -0700

    Fix wrong port for https

--- lib/demo-environments.ts
+++ lib/demo-environments.ts
@@ -31,7 +31,7 @@ export class DemoEnvironments extends Stack {
     });
     securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'Allow SSH Access')
     securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'Allow HTTP Access')
-    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(8080), 'Allow HTTPS Access')
+    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(443), 'Allow HTTPS Access')
 
     const role = new iam.Role(this, 'ec2Role', {
       assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com')
